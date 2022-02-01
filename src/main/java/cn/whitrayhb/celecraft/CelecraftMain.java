d9d04860c8978a421bce6ce82e7908df8308109a
WhitrayHB <2039851887@qq.com>
2022-02-01 17:31:09 +0800

    Berry Sound pt3

--- src/main/java/cn/whitrayhb/celecraft/CelecraftMain.java
+++ src/main/java/cn/whitrayhb/celecraft/CelecraftMain.java
@@ -28,18 +28,50 @@ public class CelecraftMain implements ModInitializer {
 	/*SoundEvents*/
 	public static final Identifier STRAWBERRY_GET_1000_ID = new Identifier("celecraft:strawberry_get_1000");
 	public static SoundEvent STRAWBERRY_GET_1000 = new SoundEvent(STRAWBERRY_GET_1000_ID);
+	public static final Identifier STRAWBERRY_GET_2000_ID = new Identifier("celecraft:strawberry_get_2000");
+	public static SoundEvent STRAWBERRY_GET_2000 = new SoundEvent(STRAWBERRY_GET_2000_ID);
+	public static final Identifier STRAWBERRY_GET_3000_ID = new Identifier("celecraft:strawberry_get_3000");
+	public static SoundEvent STRAWBERRY_GET_3000 = new SoundEvent(STRAWBERRY_GET_3000_ID);
+	public static final Identifier STRAWBERRY_GET_4000_ID = new Identifier("celecraft:strawberry_get_4000");
+	public static SoundEvent STRAWBERRY_GET_4000 = new SoundEvent(STRAWBERRY_GET_4000_ID);
+	public static final Identifier STRAWBERRY_GET_5000_ID = new Identifier("celecraft:strawberry_get_5000");
+	public static SoundEvent STRAWBERRY_GET_5000 = new SoundEvent(STRAWBERRY_GET_5000_ID);
+	public static final Identifier STRAWBERRY_GET_1up_ID = new Identifier("celecraft:strawberry_get_1up");
+	public static SoundEvent STRAWBERRY_GET_1up = new SoundEvent(STRAWBERRY_GET_1up_ID);
 	public static final Identifier GOLDENBERRY_GET_1000_ID = new Identifier("celecraft:goldenberry_get_1000");
 	public static SoundEvent GOLDENBERRY_GET_1000 = new SoundEvent(GOLDENBERRY_GET_1000_ID);
+	public static final Identifier GOLDENBERRY_GET_2000_ID = new Identifier("celecraft:goldenberry_get_2000");
+	public static SoundEvent GOLDENBERRY_GET_2000 = new SoundEvent(GOLDENBERRY_GET_2000_ID);
+	public static final Identifier GOLDENBERRY_GET_3000_ID = new Identifier("celecraft:goldenberry_get_3000");
+	public static SoundEvent GOLDENBERRY_GET_3000 = new SoundEvent(GOLDENBERRY_GET_3000_ID);
+	public static final Identifier GOLDENBERRY_GET_4000_ID = new Identifier("celecraft:goldenberry_get_4000");
+	public static SoundEvent GOLDENBERRY_GET_4000 = new SoundEvent(GOLDENBERRY_GET_4000_ID);
+	public static final Identifier GOLDENBERRY_GET_5000_ID = new Identifier("celecraft:goldenberry_get_5000");
+	public static SoundEvent GOLDENBERRY_GET_5000 = new SoundEvent(GOLDENBERRY_GET_5000_ID);
+	public static final Identifier GOLDENBERRY_GET_1up_ID = new Identifier("celecraft:goldenberry_get_1up");
+	public static SoundEvent GOLDENBERRY_GET_1up = new SoundEvent(GOLDENBERRY_GET_1up_ID);
 	public static final Identifier MOONBERRY_GET_ID = new Identifier("celecraft:moonberry_get");
 	public static SoundEvent MOONBERRY_GET = new SoundEvent(MOONBERRY_GET_ID);
 	@Override
 	public void onInitialize(){
 		CELECRAFT.info("Welcome to Celecraft World!");
+		/*Item*/
 		Registry.register(Registry.ITEM,new Identifier("celecraft","strawberry"),STRAWBERRY);
 		Registry.register(Registry.ITEM,new Identifier("celecraft","goldenberry"),GOLDENBERRY);
 		Registry.register(Registry.ITEM,new Identifier("celecraft","moonberry"),MOONBERRY);
+		/*SoundEvent*/
 		Registry.register(Registry.SOUND_EVENT,STRAWBERRY_GET_1000_ID,STRAWBERRY_GET_1000);
+		Registry.register(Registry.SOUND_EVENT,STRAWBERRY_GET_2000_ID,STRAWBERRY_GET_2000);
+		Registry.register(Registry.SOUND_EVENT,STRAWBERRY_GET_3000_ID,STRAWBERRY_GET_3000);
+		Registry.register(Registry.SOUND_EVENT,STRAWBERRY_GET_4000_ID,STRAWBERRY_GET_4000);
+		Registry.register(Registry.SOUND_EVENT,STRAWBERRY_GET_5000_ID,STRAWBERRY_GET_5000);
+		Registry.register(Registry.SOUND_EVENT,STRAWBERRY_GET_1up_ID,STRAWBERRY_GET_1up);
 		Registry.register(Registry.SOUND_EVENT,GOLDENBERRY_GET_1000_ID,GOLDENBERRY_GET_1000);
+		Registry.register(Registry.SOUND_EVENT,GOLDENBERRY_GET_2000_ID,GOLDENBERRY_GET_2000);
+		Registry.register(Registry.SOUND_EVENT,GOLDENBERRY_GET_3000_ID,GOLDENBERRY_GET_3000);
+		Registry.register(Registry.SOUND_EVENT,GOLDENBERRY_GET_4000_ID,GOLDENBERRY_GET_4000);
+		Registry.register(Registry.SOUND_EVENT,GOLDENBERRY_GET_5000_ID,GOLDENBERRY_GET_5000);
+		Registry.register(Registry.SOUND_EVENT,GOLDENBERRY_GET_1up_ID,GOLDENBERRY_GET_1up);
 		Registry.register(Registry.SOUND_EVENT,MOONBERRY_GET_ID,MOONBERRY_GET);
 	}
 }
--- src/main/java/cn/whitrayhb/celecraft/item/Berry.java
+++ src/main/java/cn/whitrayhb/celecraft/item/Berry.java
@@ -4,6 +4,8 @@ import cn.whitrayhb.celecraft.CelecraftMain;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -12,20 +14,44 @@ import net.minecraft.world.World;
 public class Berry extends Item
 {
     private String type;
-    public Berry(Settings settings, String Type) {
+    private static int count = 0;
+    public Berry(Settings settings,String Type) {
         super(settings);
         this.type = Type;
     }
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand)
     {
-        switch(type)
+        if(count<11) count++;
+        if(type == "strawberry")
         {
-            case "strawberry":playerEntity.playSound(CelecraftMain.STRAWBERRY_GET_1000,1.0F,1.0F);break;
-            case "goldenberry":playerEntity.playSound(CelecraftMain.GOLDENBERRY_GET_1000,1.0f,1.0f);break;
-            case "moonberry":playerEntity.playSound(CelecraftMain.MOONBERRY_GET,1.0f,1.0f);
-            default:
+            switch (count){
+                case 1:playerEntity.playSound(CelecraftMain.STRAWBERRY_GET_1000, 1.0F, 1.0F);break;
+                case 3:playerEntity.playSound(CelecraftMain.STRAWBERRY_GET_2000, 1.0F, 1.0F);break;
+                case 5:playerEntity.playSound(CelecraftMain.STRAWBERRY_GET_3000, 1.0F, 1.0F);break;
+                case 7:playerEntity.playSound(CelecraftMain.STRAWBERRY_GET_4000, 1.0F, 1.0F);break;
+                case 9:playerEntity.playSound(CelecraftMain.STRAWBERRY_GET_5000, 1.0F, 1.0F);break;
+                case 11:playerEntity.playSound(CelecraftMain.STRAWBERRY_GET_1up, 1.0F, 1.0F);
+            }
         }
+        if(type=="goldenberry")
+        {
+            switch (count){
+                case 1:playerEntity.playSound(CelecraftMain.GOLDENBERRY_GET_1000,1.0F,1.0F);break;
+                case 3:playerEntity.playSound(CelecraftMain.GOLDENBERRY_GET_2000,1.0F,1.0F);break;
+                case 5:playerEntity.playSound(CelecraftMain.GOLDENBERRY_GET_3000,1.0F,1.0F);break;
+                case 7:playerEntity.playSound(CelecraftMain.GOLDENBERRY_GET_4000,1.0F,1.0F);break;
+                case 9:playerEntity.playSound(CelecraftMain.GOLDENBERRY_GET_5000,1.0F,1.0F);break;
+                case 11:playerEntity.playSound(CelecraftMain.GOLDENBERRY_GET_1up,1.0F,1.0F);
+            }
+        }
+        //我也不知道为什么他会是1357的计数方式，但是能跑就行√
+        if(type=="moonberry") playerEntity.playSound(CelecraftMain.MOONBERRY_GET,1.0f,1.0f);
         return new TypedActionResult<>(ActionResult.SUCCESS,playerEntity.getStackInHand(hand));
+    }
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        count=0;
+        return ActionResult.SUCCESS;
     }
 }
